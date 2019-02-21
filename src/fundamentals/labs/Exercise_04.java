4a344ab06d32a2a74b61f18bc5bf3f9f4fc01dad
Asem <asemsoufi@gmail.com>
2019-02-21 12:16:58 +0000

    completed exercise 04

--- src/fundamentals/labs/Exercise_04.java
+++ src/fundamentals/labs/Exercise_04.java
@@ -1,5 +1,6 @@
 package fundamentals.labs;
 
+
 /**
  * Fundamentals Exercise 4: Volume and Surface Area
  *
@@ -13,6 +14,15 @@ public class Exercise_04 {
     public static void main(String[] args) {
 
         // write code here
+        float h = 5f;
+        float r = 3.14f;
+        double volume = Math.PI * (r * r) * h;
+        double surface = Math.PI * 2 * r * h;
+
+        System.out.println("We have a cylinder with a radius of 3.14 and a height of 5.");
+        System.out.println(" The volume of this cylinder is " + volume);
+        System.out.println(" The surface of this cylinder is " + surface);
+
 
     }
 }
