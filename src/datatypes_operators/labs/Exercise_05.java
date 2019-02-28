29d47619e3b5d2699736e9b2b1abcf2efb57163b
Asem <asemsoufi@gmail.com>
2019-02-28 22:58:54 +0000

    completed exercise 05, datatypes_operators

--- src/datatypes_operators/labs/Exercise_05.java
+++ src/datatypes_operators/labs/Exercise_05.java
@@ -13,11 +13,38 @@ class LogicalOperators {
         // example of "OR"
         boolean a = true;
         boolean b = false;
+
         if (a | b){
-            System.out.println("a or b is true");
+            System.out.println("a | b is True, because either a or b is true, but I can't tell which!");
         }
+        System.out.println();
 
         // write your code below
+
+        if (a || b){
+            System.out.println("a || b is True! because a is true, " +
+                    "but I'm not sure about b as it was never evaluated!");
+        }
+        System.out.println();
+        System.out.println("Using NOT to force the printing of the following:");
+        if (!(a & b)){
+            System.out.println("a & b is False! " +
+                    "After checking both a & b and finding them to be contradictory.");
+        }
+        System.out.println();
+
+        if (!(a && b)){
+            System.out.println("Using NOT to force the printing of the following:");
+            System.out.println("a && b is False! " +
+                    "After checking a first and finding it to be true, " +
+                    "I then moved to check b but it was false!");
+        }
+        System.out.println();
+
+        if (a ^ b){
+            System.out.println("a XOR b is True! because only one of them is true.");
+        }
+
 
     }
 
