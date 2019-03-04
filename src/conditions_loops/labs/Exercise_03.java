48b77ef8eaebf2368c7b6d07af22ff3c04cfc68f
Asem <asemsoufi@gmail.com>
2019-03-04 17:48:57 +0000

    completed exercise 02 & 03, conditions_loops.

--- src/conditions_loops/labs/Exercise_03.java
+++ src/conditions_loops/labs/Exercise_03.java
@@ -1,5 +1,7 @@
 package conditions_loops.labs;
 
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 3: Months of the year
  *
@@ -10,4 +12,55 @@ package conditions_loops.labs;
 
 public class Exercise_03 {
 
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int num = scanner.nextInt();
+
+        String month;
+        switch (num) {
+            case 1:
+                month = "January";
+                break;
+            case 2:
+                month = "February";
+                break;
+            case 3:
+                month = "March";
+                break;
+            case 4:
+                month = "April";
+                break;
+            case 5:
+                month = "May";
+                break;
+            case 6:
+                month = "June";
+                break;
+            case 7:
+                month = "July";
+                break;
+            case 8:
+                month = "August";
+                break;
+            case 9:
+                month = "September";
+                break;
+            case 10:
+                month = "October";
+                break;
+            case 11:
+                month = "November";
+                break;
+            case 12:
+                month = "December";
+                break;
+            default:
+                month = "Other";
+        }
+
+        System.out.println("The month you chose is " + month);
+
+    }
 }
